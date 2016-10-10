5da0f6610ff6e2483ee7aef976200eb05cd35da1
Fabiano Amaral <fabianin.amaral@gmail.com>
2016-10-10 13:36:33 -0300

    first commit

---
+++ src/main/java/br/ufes/ceunes/projeta/Projeta1Application.java
@@ -0,0 +1,12 @@
+package br.ufes.ceunes.projeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeta1Application.class, args);
+	}
+}
---
+++ src/main/java/br/ufes/ceunes/projeta/controllers/Home.java
@@ -0,0 +1,23 @@
+package br.ufes.ceunes.projeta.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class Home {
+
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public ModelAndView home(){
+		return new ModelAndView("home");
+		
+	}
+}
