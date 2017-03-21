2e666a2ac4c2022cc28b5ea9343981a685acb6cf
Fabiano Amaral <fabianin.amaral@gmail.com>
2017-03-21 20:00:41 -0300

    ...

--- src/main/java/br/ufes/ceunes/projeta/Projeta1Application.java
+++ src/main/java/br/ufes/ceunes/projeta/Projeta1Application.java
@@ -2,9 +2,15 @@ package br.ufes.ceunes.projeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Projeta1Application {
+	
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Projeta1Application.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeta1Application.class, args);
